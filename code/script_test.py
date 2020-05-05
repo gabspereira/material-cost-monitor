67d85aacc3a950ddd76fe56df34ae8db602329c1
gabspereira <contato.gabrielfaria@gmail.com>
2020-05-05 18:18:20 -0300

    usefull function implemented: LookUp data

--- code/script_test.py
+++ code/script_test.py
@@ -1,8 +1,11 @@
 #script_test.py
 import sys, os
-cwd = os.getcwd() + '/06 material-cost-monitor/material-cost-monitor/src'
+cwd = os.getcwd() + '/material-cost-monitor/src'
 sys.path.insert(1, cwd)
 
 import custom_funcs as cf
 
 cf.SendMessage()
+
+
+cf.LookUp('simaris').raw()
--- src/custom_funcs.py
+++ src/custom_funcs.py
@@ -1,3 +1,20 @@
 def SendMessage():
-    msg = 'Test of called function, ok!'
+    msg = 'Test of called function, ok...!'
     return msg
+
+
+class LookUp():
+    def __init__(self, tool):
+        self.tool = tool
+
+    def raw(self):
+        if self.tool == 'nxtools':
+            dir_raw = 'K:/EM_MS/EM_MS_BA/Controladoria/CO_OP/Cost Controlling/Product Cost/40 Material Cost Monitor/data/raw/nxtools/FY20/'
+        elif self.tool == 'simaris':
+            dir_raw = 'K:/EM_MS/EM_MS_BA/Controladoria/CO_OP/Cost Controlling/Product Cost/40 Material Cost Monitor/data/raw/simaris/SCF2.4/'
+        elif self.tool == 'salesforce':
+            dir_raw = 'K:/EM_MS/EM_MS_BA/Controladoria/CO_OP/Cost Controlling/Product Cost/40 Material Cost Monitor/data/raw/salesforce/2020_05_05/'
+        else:
+            print('Available options: nxtools, simaris, salesforce')
+            dir_raw = 'Test: Error to get dir_raw.'
+        return dir_raw
